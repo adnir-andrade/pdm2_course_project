a16b19d59c488366c0b2f1a0499ce562ba6c94f4
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-03 20:55:54 -0300

    [PDM2-2] Add icon as placeholder

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import "./global.css";
+import Icon from "react-native-ico-rpg-game";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-2xl">Open up App.tsx to start working on your app!</Text>
+        <Icon name="wyvern" height="120" width="120"></Icon>
       <StatusBar style="auto" />
     </View>
   );
