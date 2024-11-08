021badd0fb9ab9b02dcc8d1f076929131429990f
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-07 21:20:26 -0300

    Re-Style Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,23 @@
-import { TouchableOpacity, TouchableOpacityProps } from 'react-native';
-import React from 'react';
-import Text from '../Text';
+import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import React from "react";
 
 type ImageProps = {
   title: string;
   onPress: () => void;
 } & TouchableOpacityProps;
 
-export default function ImageButton({ title, onPress }: ImageProps) {
+export default function Button({ title, onPress }: ImageProps) {
+  const roundedBorders =
+    "rounded-tl-[35] rounded-br-[35] rounded-bl-[15] rounded-tr-[15]";
+
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>{title}</Text>
+    <TouchableOpacity
+      className={`w-248 h-[80] px-4 py-2 bg-themys-dune border border-themys-straw gap-1 justify-center ${roundedBorders}`}
+      onPress={onPress}
+    >
+      <Text className="text-center text-themys-straw text-4xl font-cinzel-decorative-bold">
+        {title}
+      </Text>
     </TouchableOpacity>
   );
 }
