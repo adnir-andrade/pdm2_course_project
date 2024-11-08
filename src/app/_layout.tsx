991c748a2e3731ca70560952b2861dc7c596115f
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-07 21:00:13 -0300

    Refactor load Fonts logic inside hook

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,46 +1,11 @@
 import { Stack } from 'expo-router';
-import '../../global.css'
-import {
-    useFonts,
-    Cinzel_400Regular,
-    Cinzel_500Medium,
-    Cinzel_600SemiBold,
-    Cinzel_700Bold,
-    Cinzel_800ExtraBold,
-    Cinzel_900Black,
-} from '@expo-google-fonts/cinzel';
-import {
-    CinzelDecorative_400Regular,
-    CinzelDecorative_700Bold,
-    CinzelDecorative_900Black,
-} from '@expo-google-fonts/cinzel-decorative';
-import * as SplashScreen from 'expo-splash-screen';
-import {useEffect} from "react";
-
-SplashScreen.preventAutoHideAsync();
+import '../../global.css';
+import useLoadFonts from '../hooks/useFonts';
 
 export default function Layout() {
-    const [fontsLoaded] = useFonts({
-        Cinzel_400Regular,
-        Cinzel_500Medium,
-        Cinzel_600SemiBold,
-        Cinzel_700Bold,
-        Cinzel_800ExtraBold,
-        Cinzel_900Black,
-        CinzelDecorative_400Regular,
-        CinzelDecorative_700Bold,
-        CinzelDecorative_900Black,
-    });
+    const fontsLoaded = useLoadFonts();
 
-    useEffect(() => {
-        if (fontsLoaded) {
-            SplashScreen.hideAsync();
-        }
-    }, [fontsLoaded]);
-
-    if (!fontsLoaded) {
-        return null;
-    }
+    if (!fontsLoaded) return null;
 
     return <Stack />;
 }
