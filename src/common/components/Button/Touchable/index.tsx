8b725fc69d6821dda8dee06ea7111591ba2dce7c
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-09 17:05:56 -0300

    [Pdm2-4] Refactor Button into Touchable and Normal version

--- src/common/components/Button/Touchable/index.tsx
+++ src/common/components/Button/Touchable/index.tsx
@@ -1,9 +1,9 @@
-import { Text, TouchableOpacityProps, View } from "react-native";
+import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
 import React from "react";
-import Touchable from "../Button/Touchable";
 
 type ImageProps = {
   title: string;
+  onPress: () => void;
 } & TouchableOpacityProps;
 
 export default function Button({ title, onPress }: ImageProps) {
@@ -11,14 +11,13 @@ export default function Button({ title, onPress }: ImageProps) {
     "rounded-tl-[35] rounded-br-[35] rounded-bl-[15] rounded-tr-[15]";
 
   return (
-    <View
+    <TouchableOpacity
       className={`w-248 h-[80] px-4 py-2 bg-themys-dune border border-themys-straw justify-center ${roundedBorders}`}
+      onPress={onPress}
     >
       <Text className="text-center text-themys-straw text-4xl font-cinzel-decorative-bold -bottom-1">
         {title}
       </Text>
-    </View>
+    </TouchableOpacity>
   );
 }
-
-Button.Touchable = Touchable;
