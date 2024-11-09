ef581add031781ed31f7b3b6b831a7e51583c19a
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-09 17:10:53 -0300

    [Pdm2-4] Refactor Normal Button to always contain a Link

--- src/app/Home/screens/HomeScreen.tsx
+++ src/app/Home/screens/HomeScreen.tsx
@@ -3,15 +3,10 @@ import React from "react";
 import Logo from "../../../common/components/Logo";
 import Button from "../../../common/components/Button";
 import GoogleLogo from "../components/GoogleLogo";
-import { Link } from "expo-router";
 
 export default function Home() {
   const signInHandler = () => {
     console.log("Sign in clicked!");
-  };
-
-  const logInHandler = () => {
-    console.log("Log in clicked!");
   };
 
   return (
@@ -21,9 +16,7 @@ export default function Home() {
       </View>
       <View className="basis-4/6 justify-center items-center gap-10 w-full">
         <Button.Touchable title="Sign In" onPress={signInHandler} />
-        <Link href={{ pathname: "/Login/screens/LoginScreen" }}>
-          <Button title="Log In" />
-        </Link>
+        <Button title="Log In" path="/Login/screens/LoginScreen" />
       </View>
       <View className="basis-1/6 justify-center items-center">
         <Text className="font-cinzel-bold text-themys-straw text-2xl">
--- src/common/components/Button/index.tsx
+++ src/common/components/Button/index.tsx
@@ -1,23 +1,27 @@
 import { Text, TouchableOpacityProps, View } from "react-native";
 import React from "react";
 import Touchable from "../Button/Touchable";
+import { Link } from "expo-router";
 
 type ImageProps = {
   title: string;
+  path: string;
 } & TouchableOpacityProps;
 
-export default function Button({ title, onPress }: ImageProps) {
+export default function Button({ title, path }: ImageProps) {
   const roundedBorders =
     "rounded-tl-[35] rounded-br-[35] rounded-bl-[15] rounded-tr-[15]";
 
   return (
-    <View
-      className={`w-248 h-[80] px-4 py-2 bg-themys-dune border border-themys-straw justify-center ${roundedBorders}`}
-    >
-      <Text className="text-center text-themys-straw text-4xl font-cinzel-decorative-bold -bottom-1">
-        {title}
-      </Text>
-    </View>
+    <Link href={{ pathname: path }}>
+      <View
+        className={`w-248 h-[80] px-4 py-2 bg-themys-dune border border-themys-straw justify-center ${roundedBorders}`}
+      >
+        <Text className="text-center text-themys-straw text-4xl font-cinzel-decorative-bold -bottom-1">
+          {title}
+        </Text>
+      </View>
+    </Link>
   );
 }
 
