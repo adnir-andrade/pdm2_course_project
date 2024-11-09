6be22085310c6f1fb3d52893e85ae44c009eb66c
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-09 19:43:11 -0300

    [Pdm2-4] Implement Login Screen with user validation

--- src/app/Login/screens/LoginScreen.tsx
+++ src/app/Login/screens/LoginScreen.tsx
@@ -1,18 +1,75 @@
-import { Text, View } from "react-native";
-import React from "react";
+import { Alert, View } from "react-native";
+import React, { useState } from "react";
+import Background from "../../../common/components/Background";
+import Watermark from "../../../common/components/Watermark";
+import Logo from "../../../common/components/Logo";
+import Input from "../../../common/Input";
+import Button from "../../../common/components/Button";
+import { users } from "../../../mocks/MOCK_USER";
 
 export default function Login() {
-  const signInHandler = () => {
-    console.log("Sign in clicked!");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const findUserByEmail = (email: string) => {
+    console.log(email);
+    return users.find((user) => user.email === email);
   };
 
-  const logInHandler = () => {
-    console.log("Log in clicked!");
+  const checkPassword = (userPassword: string, inputPassword: string) => {
+    return userPassword === inputPassword;
+  };
+
+  const logInHandler = async () => {
+    const user = findUserByEmail(email);
+
+    if (!user) {
+      Alert.alert("Email not found!");
+      return;
+    }
+
+    const isPasswordCorrect = checkPassword(user.password, password);
+
+    if (!isPasswordCorrect) {
+      Alert.alert("Incorrect password");
+      return;
+    }
+
+    console.log("Log in successful!");
   };
 
   return (
-    <View className="flex flex-col">
-      <Text>Test</Text>
-    </View>
+    <Background>
+      <Watermark />
+      <View className="h-full justify-center items-center">
+        <Background.Transparent>
+          <View className="flex flex-col">
+            <View className="basis-1/6 justify-center items-center">
+              <Logo />
+            </View>
+            <View className="basis-4/6 justify-center items-center gap-10 w-full px-7">
+              <Input
+                label="E-mail"
+                textContentType={"emailAddress"}
+                value={email}
+                onChangeText={setEmail}
+                autoCapitalize={"none"}
+              />
+              <Input
+                label="Password"
+                textContentType="password"
+                secureTextEntry={true}
+                value={password}
+                onChangeText={setPassword}
+              />
+              <View className="w-full mt-24">
+                <Button.Slim title="LOG IN" onPress={logInHandler} />
+              </View>
+            </View>
+            <View className="basis-1/6 justify-center items-center"></View>
+          </View>
+        </Background.Transparent>
+      </View>
+    </Background>
   );
 }
