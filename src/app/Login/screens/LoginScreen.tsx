9356b075feaaf5e24ffcde871f714b786e845796
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-09 16:27:12 -0300

    Implement LoginScreen

---
+++ src/app/Login/screens/LoginScreen.tsx
@@ -0,0 +1,29 @@
+import { Text, View } from "react-native";
+import React from "react";
+import Logo from "../../../common/components/Logo";
+import Button from "../../../common/components/Button";
+import GoogleLogo from "../components/GoogleLogo";
+
+export default function Login() {
+  const buttonFunction = () => {
+    console.log("click");
+  };
+
+  return (
+    <View className="flex flex-col">
+      <View className="basis-1/6 justify-center items-center">
+        <Logo />
+      </View>
+      <View className="basis-4/6 justify-center items-center gap-10 w-full">
+        <Button title="Sign In" onPress={buttonFunction} />
+        <Button title="Log In" onPress={buttonFunction} />
+      </View>
+      <View className="basis-1/6 justify-center items-center">
+        <Text className="font-cinzel-bold text-themys-straw text-2xl">
+          Or Log In with
+        </Text>
+        <GoogleLogo />
+      </View>
+    </View>
+  );
+}
