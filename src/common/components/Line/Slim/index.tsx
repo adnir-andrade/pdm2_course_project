6531550eef189862cfd726331093e44fbcf8331f
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-10 10:31:08 -0300

    [Pdm2-6] Adjust Slim Line max-h

--- src/common/components/Line/Slim/index.tsx
+++ src/common/components/Line/Slim/index.tsx
@@ -2,5 +2,5 @@ import { View } from "react-native";
 import React from "react";
 
 export default function Line() {
-  return <View className="flex-1 h-[0.5] bg-themys-straw" />;
+  return <View className="flex-1 h-0.5 max-h-0.5 bg-themys-straw" />;
 }
