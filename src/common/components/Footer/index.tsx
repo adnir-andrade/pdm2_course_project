6fbe3648e226ee9fcd133f3ba8040f68e625f34c
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-09 23:38:18 -0300

    [Pdm2-5] Re-style common Footer component

--- src/common/components/Footer/index.tsx
+++ src/common/components/Footer/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Text from "../Text";
-import { CardBase } from "@rneui/base/dist/Card/Card";
+import { View } from "react-native";
 
 export default function Footer() {
+  const baseStyle =
+    "text-center text-shadow-lg text-themys-tussock tracking-widest";
+
   return (
-    <CardBase>
-      <Text extraClass="text-base text-shadow-lg">
-        Developed by Adnir Andrade @ 2024
-      </Text>
-    </CardBase>
+    <View className="flex-row w-full h-[5%] absolute bottom-0 bg-themys-cod-gray justify-center items-center opacity-85">
+      <Text className={`${baseStyle} font-cinzel-regular`}>Developed by [</Text>
+      <Text className={`${baseStyle} font-cinzel-bold`}>Adnir Andrade</Text>
+      <Text className={`${baseStyle} font-cinzel-regular`}>] @ 2024</Text>
+    </View>
   );
 }
