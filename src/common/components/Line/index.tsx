f96f3783168f1a8b40f09d04268a8ebe889d5fce
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-10 00:19:47 -0300

    [Pdm2-6] Re-style standard line component

--- src/common/components/Line/index.tsx
+++ src/common/components/Line/index.tsx
@@ -1,14 +1,29 @@
-import { Image } from "react-native";
+import { View } from "react-native";
 import Slim from "./Slim";
 import React from "react";
 
-export default function Line() {
+type LineProp = {
+  inverted?: boolean;
+};
+
+export default function Line({ inverted = false }: LineProp) {
+  const line = "flex-1 h-1 bg-themys-straw shadow-basic";
+  const diamond = "h-3 w-3 bg-themys-straw rotate-45 shadow-glow";
+
   return (
-    <Image
-      source={require("../../../../assets/images/Decoration.png")}
-      className="absolute top-0 w-[66%] h-full"
-      resizeMode="contain"
-    />
+    <>
+      {inverted ? (
+        <>
+          <View className={line} />
+          <View className={diamond} />
+        </>
+      ) : (
+        <>
+          <View className={diamond} />
+          <View className={line} />
+        </>
+      )}
+    </>
   );
 }
 
