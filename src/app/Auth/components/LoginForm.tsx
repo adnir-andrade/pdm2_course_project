9f25ef583f1c19ae3de1b29598f67c10e532d4ff
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-10 19:03:16 -0300

    [Pdm2-9] Refactor main Button usage and refactor usage

--- src/app/Auth/components/LoginForm.tsx
+++ src/app/Auth/components/LoginForm.tsx
@@ -22,7 +22,7 @@ export default function LoginForm() {
     if (success) {
       login(user!.username, token!);
       router.dismissAll();
-      router.replace("(Restricted)/");
+      router.replace("(Restricted)/_navigation/Drawer");
       return;
     }
 
--- src/app/Home/components/HomeContent.tsx
+++ src/app/Home/components/HomeContent.tsx
@@ -16,7 +16,7 @@ export default function HomeContent() {
       </View>
       <View className="basis-4/6 justify-center items-center gap-10 w-full">
         <Button.Touchable title="Sign In" onPress={signInHandler} />
-        <Button title="Log In" path="/Auth/screens/LoginScreen" />
+        <Button title="Log In" path="Auth/screens/LoginScreen" />
       </View>
       <View className="basis-1/6 justify-center items-center gap-3">
         <Footer />
