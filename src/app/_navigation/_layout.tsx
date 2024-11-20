5cf010508fbd0f766dfa630cfe674a1f71febc14
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-20 20:45:14 -0300

    [Pdm2-12] WIP: First step at implementing a custom Tab

--- src/app/_navigation/_layout.tsx
+++ src/app/_navigation/_layout.tsx
@@ -1,8 +1,49 @@
 import { Tabs } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function RestrictedLayout() {
+  const routeNames: { [key: string]: string } = {
+    index: "Home",
+    DM: "Campaigns",
+    Player: "Campaigns",
+  };
+
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#c29f41",
+        tabBarInactiveTintColor: "#cfbe86",
+        tabBarStyle: {
+          backgroundColor: "#161414",
+          borderColor: "#cfbe86",
+          shadowColor: "#cfbe86",
+          shadowOpacity: 0.4,
+          height: 60,
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+        tabBarIcon: ({ focused }) => {
+          const routeName = routeNames[route.name] || route.name;
+
+          const shadowStyle = focused ? "shadow-lg" : "";
+          return (
+            <View
+              className={`${shadowStyle} h-full w-full justify-center items-center`}
+            >
+              {/*TODO: Add icon depending on tab name. Gotta create a object for that probably*/}
+              {/*<Icon name={} />*/}
+              <Text
+                className={`text ${focused ? "text-[#c29f41] text-xl" : "text-[#cfbe86]"}`}
+              >
+                {routeName}
+              </Text>
+            </View>
+          );
+        },
+      })}
+    >
       <Tabs.Screen
         name="DM"
         options={{
