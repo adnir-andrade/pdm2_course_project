4563736c0a363d1b315ea16cccaecafba32820ed
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-20 12:26:39 -0300

    [Pdm2-11] Change Player's Stack into Drawer

--- src/app/_navigation/Player/_layout.tsx
+++ src/app/_navigation/Player/_layout.tsx
@@ -1,14 +1,18 @@
-import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
 
 export default function PlayerLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-        name="Characters"
-        options={{
-          headerTitle: "Characters",
-        }}
-      />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer screenOptions={{ headerShown: false }}>
+        <Drawer.Screen
+          name="index"
+          options={{
+            headerTitle: "Characters",
+            drawerLabel: "Characters",
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
   );
 }
