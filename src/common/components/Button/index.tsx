f187c0b74ee964089c6cd2c53e54e58bfb64bed2
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-20 17:34:11 -0300

    [Pdm2-10] Refactor Button Style

--- src/common/components/Button/index.tsx
+++ src/common/components/Button/index.tsx
@@ -9,14 +9,8 @@ type ButtonProps = {
 } & TouchableOpacityProps;
 
 export default function Button({ title, onPress }: ButtonProps) {
-  const roundedBorders =
-    "rounded-tl-[35] rounded-br-[35] rounded-bl-[15] rounded-tr-[15]";
-
   return (
-    <TouchableOpacity
-      className={`w-full h-[80] px-4 py-2 bg-themys-dune border border-themys-straw justify-center shadow-basic ${roundedBorders}`}
-      onPress={onPress}
-    >
+    <TouchableOpacity className={`button`} onPress={onPress}>
       <Text className="text-center text-themys-straw text-4xl font-cinzel-decorative-bold -bottom-1 text-shadow-lg">
         {title}
       </Text>
