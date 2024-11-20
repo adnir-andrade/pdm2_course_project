3edc9e91081a8520b0143cc091817eeb45d0ed75
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-20 16:22:09 -0300

    [Pdm2-11] Dismember button function

--- src/app/(Restricted)/(Player)/Characters/components/AddCharacter.tsx
+++ src/app/(Restricted)/(Player)/Characters/components/AddCharacter.tsx
@@ -3,12 +3,13 @@ import React from "react";
 import Button from "../../../../../common/components/Button";
 
 export default function AddCharacter() {
+  const addCharacterHandler = () => {
+    console.log(`Add Character`);
+  };
+
   return (
     <View className="mb-16">
-      <Button.Slim
-        title="New Character"
-        onPress={() => console.log(`Add Character`)}
-      />
+      <Button.Slim title="New Character" onPress={addCharacterHandler} />
     </View>
   );
 }
