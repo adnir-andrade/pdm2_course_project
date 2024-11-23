12ada735a1f812a293a8f28315bb1660f25fb1b3
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-23 00:12:56 -0300

    [Pdm2-12] Thrown away everything that I was working on, but at least is working now

--- src/common/components/Drawer/SideBar/index.tsx
+++ src/common/components/Drawer/SideBar/index.tsx
@@ -1,78 +1,70 @@
-import React, {useEffect, useState} from "react";
-import { ScrollView, View } from "react-native";
+import React from "react";
+import {View} from "react-native";
 import Profile from "./components/Profile";
 import SideBarButton from "./components/SideBarButton";
-import { DrawerContentComponentProps } from "@react-navigation/drawer";
+import {DrawerContentComponentProps, DrawerContentScrollView, DrawerItemList} from "@react-navigation/drawer";
 
 type SideBarProps = {} & DrawerContentComponentProps;
 
-type Categories = {
-  character: CategoryItem[];
-  player: CategoryItem[];
-  actions: CategoryItem[];
-};
-
-type CategoryItem = {
-  [key: string]: {
-    label: string;
-    color: ColorType;
-  };
-};
-
-type ColorType = "normal" | "golden" | "red";
-
-const categories : Categories = {
-  character: [
-    { index: { label: "Character Sheet", color: "normal" } },
-    { Inventory: { label: "Inventory", color: "normal" } },
-    { Journal: { label: "Journal", color: "normal" } },
-  ],
-  player: [
-    { CharacterSelection: { label: "Character Selection", color: "golden" } },
-    { CampaignSelection: { label: "Campaign Selection", color: "golden" } },
-    { Home: { label: "Home", color: "golden" } },
-  ],
-  actions: [
-    { LogOut: { label: "Log Out", color: "red" } },
-  ],
-};
-
-export default function SideBar({ state, navigation, descriptors, ...rest }: SideBarProps) {
-  const renderButtons = (category: CategoryItem[]) => {
-    return category.map((item) => {
-      const routeName = Object.keys(item)[0];
-      const { label, color } = item[routeName];
-
-      const routeIndex = state.routes.findIndex(route => route.name === routeName);
-      const isActive = state.index === routeIndex;
-
-      return (
-          <SideBarButton
-              key={routeName}
-              label={label}
-              iconColor={color}
-              isActive={isActive}
-          />
-      );
-    });
-  };
-
+export default function SideBar({ state, descriptors, ...props }: SideBarProps) {
   return (
     <View className={`flex h-full bg-themys-cod-gray shadow-lg mb-96`}>
       <View className={`basis-[40%] items-center`}>
         <Profile />
       </View>
-      <ScrollView className={`h-full`} bounces={false}>
+      <DrawerContentScrollView className={`h-full`} bounces={false}>
         <View className={`gap-1 mb-20`}>
-          {renderButtons(categories.character)}
+          <SideBarButton
+              label={"Character Sheet"}
+              iconColor={"normal"}
+              isActive={true}
+              navigation={props.navigation}
+              route={"index"}
+          />
+          <SideBarButton
+              label={"Inventory"}
+              iconColor={"normal"}
+              isActive={true}
+              navigation={props.navigation}
+              route={"Inventory"}
+          />
+          <SideBarButton
+              label={"Journal"}
+              iconColor={"normal"}
+              isActive={true}
+              navigation={props.navigation}
+              route={"Journal"}
+          />
         </View>
         <View className={`gap-1 mb-10`}>
-          {renderButtons(categories.player)}
+          <SideBarButton
+              label={"Character Selection"}
+              iconColor={"golden"}
+              navigation={props.navigation}
+              route={"CharacterSelection"}
+          />
+          <SideBarButton
+              label={"Campaign Selection"}
+              iconColor={"golden"}
+              navigation={props.navigation}
+              route={"CampaignSelection"}
+          />
+          <SideBarButton
+              label={"Home"}
+              iconColor={"golden"}
+              navigation={props.navigation}
+              route={"Home"}
+          />
         </View>
         <View className={`mb-2`}>
-          {renderButtons(categories.actions)}
+          <SideBarButton
+              label={"Log Out"}
+              iconColor={"red"}
+              navigation={props.navigation}
+              route={"LogOut"}
+          />
         </View>
-      </ScrollView>
+      </DrawerContentScrollView>
     </View>
   );
 }
