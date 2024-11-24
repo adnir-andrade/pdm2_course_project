1e2123b5b8b2ece79e6679f348852b222aba1a25
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-24 14:43:47 -0300

    [Pdm2-14] Adjust Inventory Screen

--- src/app/(Restricted)/(Player)/(Dashboard)/Inventory/screens/InventoryScreen.tsx
+++ src/app/(Restricted)/(Player)/(Dashboard)/Inventory/screens/InventoryScreen.tsx
@@ -2,23 +2,22 @@ import { View } from "react-native";
 import React from "react";
 import Background from "../../../../../../common/components/Background";
 import Watermark from "../../../../../../common/components/Watermark";
-import Footer from "../../../../../../common/components/Footer";
 import Text from "../../../../../../common/components/Text";
+import InventoryContent from "../components/InventoryContent";
 
 export default function InventoryScreen() {
   return (
     <Background>
       <Watermark />
       <View className="flex-1 flex-col">
-        <View className="basis-[15%] w-full px-1 centralized">
-          <Text.Title>Inventory</Text.Title>
+        <View className="basis-[15%] w-full px-1 pt-14 centralized">
+          <Text.Title>Player Name</Text.Title>
         </View>
         <View className="items-center h-full max-h-[90%]">
           <Background.Transparent>
-            <Text>Inventory Screen</Text>
+            <InventoryContent className={`px-5 py-8`} />
           </Background.Transparent>
         </View>
-        <Footer />
       </View>
     </Background>
   );
