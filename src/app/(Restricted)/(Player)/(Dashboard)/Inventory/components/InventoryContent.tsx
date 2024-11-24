93f8e0e5b2eeff06bba919bab366174ccd760cc6
Adnir Andrade <adnir_andrade@hotmail.com>
2024-11-24 15:39:22 -0300

    [Pdm2-14] Finish implementing Inventory Content

---
+++ src/app/(Restricted)/(Player)/(Dashboard)/Inventory/components/InventoryContent.tsx
@@ -0,0 +1,21 @@
+import { View, ViewProps } from "react-native";
+import React from "react";
+import Card from "../../../../../../common/components/Card";
+import InventoryList from "./InventoryList"; // Atualize o caminho conforme necessário
+
+type Props = {} & ViewProps;
+
+export default function InventoryContent({ ...rest }: Props) {
+  return (
+    <View {...rest}>
+      <Card>
+        <View className="flex flex-col items-center h-full">
+          <View className="basis-[12.5%] w-full px-4">
+            <Card.Title title="Inventory" />
+          </View>
+          <InventoryList />
+        </View>
+      </Card>
+    </View>
+  );
+}
