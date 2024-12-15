e223a451ea854115993adf75a1b69859ae2b70e8
Adnir Andrade <adnir_andrade@hotmail.com>
2024-12-15 20:20:45 -0300

    [PDM2-17] Remove some mock to see more clearly

--- src/mocks/MOCK_CAMPAIGN.ts
+++ src/mocks/MOCK_CAMPAIGN.ts
@@ -17,28 +17,4 @@ export const campaigns = [
     name: "Eldoria",
     next_session: new Date("2024-11-12T18:00:00Z"),
   },
-  {
-    id: 4,
-    dm_id: 1,
-    name: "Shadowfall",
-    next_session: new Date("2024-11-22T21:00:00Z"),
-  },
-  {
-    id: 5,
-    dm_id: 1,
-    name: "Mystara",
-    next_session: new Date("2024-11-14T16:00:00Z"),
-  },
-  {
-    id: 6,
-    dm_id: 1,
-    name: "Abyssal Depths",
-    next_session: new Date("2024-11-20T19:30:00Z"),
-  },
-  {
-    id: 7,
-    dm_id: 1,
-    name: "Dragonspire",
-    next_session: new Date("2024-11-16T17:00:00Z"),
-  },
 ];
