fa133b16f45a173f3f63d7a9c9f01776af2133fe
Adnir Andrade <adnir_andrade@hotmail.com>
2024-12-15 23:31:56 -0300

    [Pdm2-17[ Fix schema import and adjust validations

--- src/schemas/Character.ts
+++ src/schemas/Character.ts
@@ -3,11 +3,12 @@ import { z } from "zod";
 const characterSchema = z.object({
   id: z.number(),
   player_id: z.number(),
-  name: z.string(),
+  name: z.string().nonempty(),
   level: z.number(),
-  gender: z.string(),
+  gender: z.string().nullish(),
   race_id: z.number(),
   klass_id: z.number(),
 });
 
+export { characterSchema };
 export type Character = z.infer<typeof characterSchema>;
