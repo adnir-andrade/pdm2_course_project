042c28a7d6fed058156b2135f70f6ffc0484a36a
Adnir Andrade <adnir_andrade@hotmail.com>
2024-12-17 00:03:41 -0300

    [Pdm2-20] Refactor code

--- src/helpers/dateHelpers.ts
+++ src/helpers/dateHelpers.ts
@@ -1,10 +1,8 @@
 export const formatDate = (date: Date): string => {
-  const daysOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-  const dayOfWeek = daysOfWeek[date.getDay()];
+  const dayAbbreviation = date
+    .toLocaleDateString(undefined, { weekday: "short" })
+    .toUpperCase();
+  const formattedDate = date.toLocaleDateString();
 
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  const year = date.getFullYear();
-
-  return `${month}/${day}/${year}, ${dayOfWeek}`;
+  return `${formattedDate}, ${dayAbbreviation}`;
 };
