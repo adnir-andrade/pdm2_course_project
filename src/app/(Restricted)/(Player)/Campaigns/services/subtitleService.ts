0520cfd108ab87dcd1f42e9b5ec161d4f0136039
Adnir Andrade <adnir_andrade@hotmail.com>
2024-12-17 00:03:30 -0300

    [Pdm2-20] Remove service because it doesn't make sense

--- src/app/(Restricted)/(Player)/Campaigns/services/subtitleService.ts
+++
@@ -1,9 +0,0 @@
-export const formatDate = (date: Date): string => {
-  const formattedDate = date.toLocaleDateString();
-  const dayAbbreviation = date
-    .toLocaleDateString(undefined, {
-      weekday: "short",
-    })
-    .toUpperCase();
-  return `${formattedDate}, ${dayAbbreviation}`;
-};
