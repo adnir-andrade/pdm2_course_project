d6fec6c3aa61cc9a4df24da01542c9c3c53281b6
Adnir Andrade <adnir_andrade@hotmail.com>
2024-12-16 22:43:53 -0300

    [Pdm2-20] Add user ID to userState to make queries easier

--- src/states/useUserState.tsx
+++ src/states/useUserState.tsx
@@ -1,20 +1,22 @@
 import { create } from "zustand";
 
 type State = {
+  userId: string;
   username: string;
   token: string | null;
 };
 
 type Actions = {
-  login: (newUsername: string, token: string) => void;
+  login: (newUsername: string, token: string, userId: string) => void;
   logout: () => void;
 };
 
 const useUserState = create<State & Actions>((set) => ({
+  userId: "",
   username: "No Active User",
   token: null,
-  login: (newUsername: string, token: string) => {
-    set({ username: newUsername, token: token });
+  login: (newUsername: string, token: string, userId: string) => {
+    set({ username: newUsername, token: token, userId: userId });
   },
   logout: () => {
     set({ username: "", token: null });
