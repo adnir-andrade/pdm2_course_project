b62fe05f2341e69a17817092b7b78414d63c8c1b
Adnir Andrade <adnir_andrade@hotmail.com>
2024-12-19 10:18:35 -0300

    [Pdm2-22] Add delete functionality to Entry List

--- src/app/(Restricted)/(Player)/(Dashboard)/Journal/components/EntriesList/EntryContent/index.tsx
+++ src/app/(Restricted)/(Player)/(Dashboard)/Journal/components/EntriesList/EntryContent/index.tsx
@@ -3,14 +3,14 @@ import React from "react";
 import UtilitiesButtons from "./UtilitiesButtons";
 import EntryText from "./EntryText";
 
-type Props = { content: string; id: number };
+type Props = { content: string; id: string; refreshCallback: () => void };
 
-export default function EntryContent({ content, id }: Props) {
+export default function EntryContent({ content, id, refreshCallback }: Props) {
   return (
     <View className="px-2 pb-2 -mt-5">
       <EntryText content={content} />
       <View className={`line-faded my-4`} />
-      <UtilitiesButtons id={id} />
+      <UtilitiesButtons id={id} refreshCallback={refreshCallback} />
     </View>
   );
 }
