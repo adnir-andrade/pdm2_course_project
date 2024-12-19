fb370cf6ad0cfde35d4615b1d16fc5aa628d56ff
Adnir Andrade <adnir_andrade@hotmail.com>
2024-12-18 21:50:16 -0300

    [Pdm2-22] Implement changes to load infro from API

--- src/app/(Restricted)/(Player)/(Dashboard)/Inventory/components/InventoryList/index.tsx
+++ src/app/(Restricted)/(Player)/(Dashboard)/Inventory/components/InventoryList/index.tsx
@@ -1,18 +1,31 @@
-import { FlatList, ListRenderItem, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  ListRenderItem,
+  View,
+} from "react-native";
 import React from "react";
-import { inventoryItems } from "../../../../../../../mocks/MOCK_ITEMS";
 import ListHeader from "./components/ListHeader";
 import ListLine from "./components/ListLine";
-
-type Items = {
-  id: number;
-  name: string;
-  quantity: number;
-  weight: number;
-};
+import { Item } from "../../../../../../../schemas/Item";
+import useUserState from "../../../../../../../states/useUserState";
+import useCharacterState from "../../../../../../../states/useCharacterState";
+import { useItems } from "../../../../../../../hooks/useItems";
 
 export default function InventoryList() {
-  const renderItem: ListRenderItem<Items> = ({ item }) => (
+  const { token } = useUserState();
+  const { character } = useCharacterState();
+  const { items, loading } = useItems(character?.id!, token!);
+
+  if (loading) {
+    return (
+      <View className={`flex-1 centralized bg-black`}>
+        <ActivityIndicator size={`large`} />
+      </View>
+    );
+  }
+
+  const renderItem: ListRenderItem<Item> = ({ item }) => (
     <ListLine
       id={item.id}
       name={item.name}
@@ -27,7 +40,7 @@ export default function InventoryList() {
     <>
       <ListHeader />
       <FlatList
-        data={inventoryItems}
+        data={items}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
         ItemSeparatorComponent={renderSeparator}
